fe065568fbe341c2b89dbd96f1e1b724cc6cd640
Shota Matsubara <baramatsubonzo@gmail.com>
2025-05-01 20:18:25 +1000

    Refactor: Separate staff menu view for readability

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ class Program
         while (!backToMain)
         {
             Console.Clear();
-            ShowStaffMenu();
+            StaffMenuView.Show();
             string choice = Console.ReadLine();
             Console.WriteLine();
 
@@ -198,23 +198,6 @@ class Program
                 Console.ReadLine();
             }
         }
-    }
-
-    static void ShowStaffMenu()
-    {
-        Console.WriteLine("-----------------------------------------");
-        Console.WriteLine("Staff Menu");
-        Console.WriteLine("-----------------------------------------");
-        Console.WriteLine();
-        Console.WriteLine("1. Add DVDs to system");
-        Console.WriteLine("2. Remove DVDs from system");
-        Console.WriteLine("3. Register a new member to system");
-        Console.WriteLine("4. Remove a registered member from system");
-        Console.WriteLine("5. Find a member contact phone number, given the member's name");
-        Console.WriteLine("6. Find members who are currently renting a particular movie");
-        Console.WriteLine("0. Return to main menu");
-        Console.WriteLine();
-        Console.Write("Enter your choice ==> ");
     }
 
     static void ShowMemberMenuLoop()
